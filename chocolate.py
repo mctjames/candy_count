9eca071544a1321909ba529bcc90cf1e85b15bec
James McTaggart <mctaggartjames@gmail.com>
2018-09-24 20:28:53 -0700

    Finished part C

--- chocolate.py
+++ chocolate.py
@@ -1,4 +1,8 @@
 # chocolate project cmpt 120
+############# consider changing values from float or at least have key values round down to 2 decimal spots ###############################
+
+
+
 
 #The first portion introduces the customer to the company and asks them for their name.
 
@@ -40,7 +44,7 @@ else:
 	print (almond, pb, oval, round_box)
 
 # Stage B - Box to candy calculations
-############ Need to add option to skip this step ################ sept 23
+
 print (f_name, "you can calculate your total by the number of chocolates or the number of boxes.")
 by_boxes = input("Would you like to calculate your total by the number of boxes? [Y/N]: ").upper()
 
@@ -57,11 +61,13 @@ if by_boxes == "Y":
 
 	#these calculations refer to the oval boxes
 	num_almond_oval  = float(oval_total)      * 11
-	num_pb_oval      = float(oval_total)      * 11 
+	num_pb_oval      = float(oval_total)      * 4 
 
-	cost_almond_oval = num_almond_oval        * almond
-	cost_pb_oval     = num_pb_oval            * pb
-	cost_oval_boxes  = float(oval_total)      * oval
+	# print ("Tracer num_almond_oval:", num_almond_oval, "num_pb_oval:", num_pb_oval)
+
+	cost_almond_oval = num_almond_oval        * float(almond)
+	cost_pb_oval     = num_pb_oval            * float(pb)
+	cost_oval_boxes  = float(oval_total)      * float(oval)
 
 	total_cost_oval_boxes  = cost_almond_oval + cost_pb_oval + cost_oval_boxes
 
@@ -69,24 +75,48 @@ if by_boxes == "Y":
 	num_almond_round  = float(round_box_total)  * 7
 	num_pb_round      = float(round_box_total)  * 4
 
-	cost_almond_round = num_almond_round        * almond
-	cost_pb_round     = num_pb_round            * pb
+	cost_almond_round = num_almond_round        * float(almond)
+	cost_pb_round     = num_pb_round            * float(pb)
 	cost_round_boxes  = float(round_box_total)  * float(round_box)
 
 	total_cost_round_boxes = cost_almond_round + cost_pb_round + cost_round_boxes
 
-	print ("The total cost of the oval boxes including chocolates was:", total_cost_oval_boxes)
-	print ("The total cost of the round boxes including chocolates was:", total_cost_round_boxes)
-
-
-################ currently the oval boxes arent adding up correctly but the round boxes are. Time to investigate.
-
+	print ("\nThe total cost of the oval boxes including chocolates was: $", total_cost_oval_boxes)
+	print ("To be more specific we need", num_almond_oval, "almond chocolates and", num_pb_oval, "peanut butter chocolates.")
+	print ("\nThe total cost of the round boxes including chocolates was: $", total_cost_round_boxes)
+	print ("Specifically we need", num_almond_round, "almond chocolates and", num_pb_round, "peanut butter chocolates.")
 
 else:
-	print ("Ahhh. A client with a decerning eye. Excellent")
+	print ("Excellent let us move onto selections by the number of individual chocolates.")
 
 # Part C From Bonbons to Boxes
 
 
+almond_selection = input("\nPlease select the number of almond chocolates you would like in your order: ")
+pb_selection     = input("\nPlease select the number of peanut butter chocolates you would like to order: ")
 
+almond_selection_boxes = float(almond_selection) // 11
+pb_selection_boxes     = float(pb_selection)     // 4
+
+#Here I used an if statement to calculate the excess chocolates. If the order has a lot more Almond candies then peanutbutter candies we wont have any remainder
+# for the pb candies because the extra will just go into more boxes to get to the total specified by the almond quantity.
+
+if almond_selection_boxes > pb_selection_boxes:
+	print ("You will need to order", almond_selection_boxes, "oval boxes.")
+	excess_almond = float(almond_selection) % 11
+	print ("Tracer: excess_almond: ", excess_almond)
+
+# If the almond order and the pb order are similar (A=116 pb=42) then each candy requires the same number of boxes. Here the remainder of each candy is important because it wont be added to the next box.
+
+elif almond_selection_boxes == pb_selection_boxes:
+	print ("You will need to order", almond_selection_boxes, "oval boxes.")
+	excess_almond = float(almond_selection) % 11
+	excess_pb     = float(pb_selection)     % 4 
+	print ("Tracer: excess_almond: ", excess_almond, "excess_pb", excess_pb)	
+
+# Here the Pb candies are the bulk of the order (A=5 pb=27534) in cases such as this the remainder of the almond candies doesnt matter because they will be added to another box to satisfy the need for more pb candies.
+else:
+	print ("You will need to order", pb_selection_boxes, "oval boxes.")
+	excess_pb     = float(pb_selection)     % 4
+	print ("Tracer: excess_pb: ", excess_pb)
 
